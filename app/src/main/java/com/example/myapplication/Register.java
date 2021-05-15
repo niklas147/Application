2876f5a0efa5c3a61695f5d1171055994e8217f4
selinahoelzl <75076874+selinahoelzl@users.noreply.github.com>
2021-05-15 15:36:23 +0200

    updateselina: register

--- app/src/main/java/com/example/myapplication/Register.java
+++ app/src/main/java/com/example/myapplication/Register.java
@@ -15,15 +15,32 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.myapplication.databinding.ActivityRegisterBinding;
 
-public class Register extends AppCompatActivity {
+public class Register extends AppCompatActivity implements View.OnClickListener{
 
-
+    Button bRegister;
+    EditText etVorname, etNachname, etBenutzername, etPasswort, etRePasswort;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        etVorname = (EditText) findViewById(R.id.etVorname);
+        etNachname= (EditText) findViewById(R.id.etNachname);
+        etBenutzername= (EditText) findViewById(R.id.etBenutzername);
+        etPasswort= (EditText) findViewById(R.id.etPasswort);
+        etRePasswort= (EditText) findViewById(R.id.etRePasswort);
+        bRegister = (Button) findViewById(R.id.bRegister);
 
+        bRegister.setOnClickListener(this);
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.bRegister:
+
+                
+                break;
+        }
+        }
     }
 }
