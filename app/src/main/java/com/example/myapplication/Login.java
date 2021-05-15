b606adb11e53f29953dd0ab5ade4489f4095e92f
niklas147 <niklas.klose010@gmail.com>
2021-05-15 14:23:33 +0200

    Update Login Registration

---
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.myapplication.databinding.ActivityLoginBinding;
+
+public class Login extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
